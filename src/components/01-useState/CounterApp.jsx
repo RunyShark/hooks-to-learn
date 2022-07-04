a742370ebbbfa52005e271d60c422164da75a811
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-03 22:45:31 -0500

    useState

--- src/components/01-useState/CounterApp.jsx
+++ src/components/01-useState/CounterApp.jsx
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
 
 export const CounterApp = () => {
-  const [count, setCount] = useState(0);
+  const [conunt, setCount] = useState({
+    conut1: 10,
+    conut2: 20,
+    conut3: 30,
+  });
+  const { conut1, conut2, conut3 } = conunt;
   return (
     <>
-      <h1>Counter:{count}</h1>
+      <h1>Counter:{conut1}</h1>
+      <h1>Counter:{conut2}</h1>
+      <h1>Counter:{conut3}</h1>
 
       <hr />
 
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(0)}>restart</button>
+      <button
+        className="btn"
+        onClick={() => setCount({ ...conunt, conut1: conut1 + 1 })}
+      >
+        +1
+      </button>
+      <button
+        className="btn"
+        onClick={() => setCount({ ...conunt, conut2: conut2 - 1 })}
+      >
+        -1
+      </button>
+      <button
+        className="btn"
+        onClick={() => setCount({ conut1: 10, conut2: 20, conut3: 30 })}
+      >
+        restart
+      </button>
     </>
   );
 };
