9167cda4e5be8c8facd2ed314def275150fa0234
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-06 17:38:50 -0500

    test3

--- test/hooks/useForm.test.jsx
+++ test/hooks/useForm.test.jsx
@@ -2,21 +2,36 @@ import { act, renderHook } from "@testing-library/react";
 import { useForm } from "../../src/components/hooks";
 
 describe("Test in useFrom", () => {
-  test("initial useFrom must be in obj", () => {
+  const initailFrom = {
+    name: "Sergio",
+    age: 23,
+  };
+
+  test("Must return default values", () => {
     const { result } = renderHook(() => useForm());
     expect(result.current.formState).toEqual({});
   });
   test("Form must have the same structure { name: sergio } ", () => {
-    const { result } = renderHook(() => useForm({ name: "sergio" }));
-    expect(result.current.formState).toEqual({ name: "sergio" });
+    const { result } = renderHook(() => useForm(initailFrom));
+
+    expect(result.current).toEqual({
+      formState: initailFrom,
+      name: initailFrom.name,
+      age: initailFrom.age,
+      handleOnChange: expect.any(Function),
+      onResetForm: expect.any(Function),
+    });
   });
   test("Must change state the formState", () => {
-    const { result } = renderHook(() => useForm({ name: "" }));
+    const newValue = "Dario";
+    const { result } = renderHook(() => useForm({ initailFrom }));
     const { handleOnChange } = result.current;
     act(() => {
-      handleOnChange({ name: "sergio" });
+      handleOnChange({ target: { name: "name", value: newValue } });
     });
-    expect(result.current.formState).toEqual({ name: "sergio" });
+
+    expect(result.current.name).toBe(newValue);
+    expect(result.current.formState.name).toBe(newValue);
   });
   test("Must restar state = {}", () => {
     const { result } = renderHook(() => useForm({ name: "" }));
