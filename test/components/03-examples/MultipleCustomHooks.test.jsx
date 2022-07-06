ac00c3ebc637ce1ea26002165911fe0ac7f1a510
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-06 18:37:30 -0500

    test7

--- test/components/03-examples/MultipleCustomHooks.test.jsx
+++ test/components/03-examples/MultipleCustomHooks.test.jsx
@@ -1,8 +1,17 @@
+import { act, renderHook } from "@testing-library/react";
 import { render, screen } from "@testing-library/react";
 import { MultipleCustomHooks } from "../../../src/components";
 import { useFech } from "../../../src/components/hooks/useFech";
+import { useCounter } from "../../../src/components/hooks/useCounter";
+
 jest.mock("../../../src/components/hooks/useFech");
+jest.mock("../../../src/components/hooks/useCounter");
 describe("Test in <MultipleCustomHooks/>", () => {
+  const mockIncrement = jest.fn();
+  useCounter.mockReturnValue({
+    counter: 1,
+    increment: mockIncrement,
+  });
   useFech.mockReturnValue({ data: null, isLoading: true, hasError: null });
   test("Must to show the component for default", () => {
     render(<MultipleCustomHooks />);
@@ -18,5 +27,16 @@ describe("Test in <MultipleCustomHooks/>", () => {
     render(<MultipleCustomHooks />);
     expect(screen.getByText("Hola munde")).toBeTruthy();
     expect(screen.getByText("Dario")).toBeTruthy();
+  });
+  test("Must increment", () => {
+    useFech.mockReturnValue({
+      data: [{ author: "Dario", quote: "Hola munde" }],
+      isLoading: false,
+      hasError: null,
+    });
+    render(<MultipleCustomHooks />);
+    const { result } = renderHook(() => useCounter());
+    const nextButton = screen.getByRole("button");
+    fireEvent.click(nextButton);
   });
 });
