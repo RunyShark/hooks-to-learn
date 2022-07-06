a74d44d05f2ed50ce04eee8d8c4ec4fe65225d6a
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-06 18:29:47 -0500

    test6

--- test/components/03-examples/MultipleCustomHooks.test.jsx
+++ test/components/03-examples/MultipleCustomHooks.test.jsx
@@ -1,9 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import { MultipleCustomHooks } from "../../../src/components";
+import { useFech } from "../../../src/components/hooks/useFech";
+jest.mock("../../../src/components/hooks/useFech");
 describe("Test in <MultipleCustomHooks/>", () => {
+  useFech.mockReturnValue({ data: null, isLoading: true, hasError: null });
   test("Must to show the component for default", () => {
     render(<MultipleCustomHooks />);
     expect(screen.getByText("Loading..."));
     expect(screen.getByText("BreakingBad Qoutes"));
+  });
+  test("Must to show in Quote", () => {
+    useFech.mockReturnValue({
+      data: [{ author: "Dario", quote: "Hola munde" }],
+      isLoading: false,
+      hasError: null,
+    });
+    render(<MultipleCustomHooks />);
+    expect(screen.getByText("Hola munde")).toBeTruthy();
+    expect(screen.getByText("Dario")).toBeTruthy();
   });
 });
